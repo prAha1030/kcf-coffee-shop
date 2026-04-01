9ab634104d12fd3d67d66510272fdfc441d45147
곽현민 <hunmin111@gmail.com>
2026-04-01 19:33:40 +0900

    feat: user 엔티티 추가

---
+++ src/main/java/com/kcfcoffeeshop/domain/user/entity/User.java
@@ -0,0 +1,40 @@
+package com.kcfcoffeeshop.domain.user.entity;
+
+import com.kcfcoffeeshop.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    private String address;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
