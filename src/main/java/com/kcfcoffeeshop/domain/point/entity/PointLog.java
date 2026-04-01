254a0dff4dac55b21521c22b3fc1abec21140822
곽현민 <hunmin111@gmail.com>
2026-04-01 21:03:01 +0900

    feat: point log 엔티티 추가

---
+++ src/main/java/com/kcfcoffeeshop/domain/point/entity/PointLog.java
@@ -0,0 +1,31 @@
+package com.kcfcoffeeshop.domain.point.entity;
+
+import com.kcfcoffeeshop.common.entity.BaseEntity;
+import com.kcfcoffeeshop.domain.point.enums.PointLogStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@Entity
+@Table(name = "point_logs")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PointLog extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long point_id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PointLogStatus status;
+}
---
+++ src/main/java/com/kcfcoffeeshop/domain/point/enums/PointLogStatus.java
@@ -0,0 +1,8 @@
+package com.kcfcoffeeshop.domain.point.enums;
+
+public enum PointLogStatus {
+    CHARGE,        // 충전
+    CHARGE_CANCEL, // 충전 취소
+    PAYMENT,       // 결제
+    REFUND         // 환불
+}
