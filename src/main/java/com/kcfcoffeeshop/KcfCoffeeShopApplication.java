1ab90f2799bd10e1282e986d4eba15965e552674
곽현민 <hunmin111@gmail.com>
2026-04-01 16:49:33 +0900

    init

---
+++ src/main/java/com/kcfcoffeeshop/KcfCoffeeShopApplication.java
@@ -0,0 +1,13 @@
+package com.kcfcoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcfCoffeeShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcfCoffeeShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kcfcoffeeshop/KcfCoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcfcoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcfCoffeeShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
