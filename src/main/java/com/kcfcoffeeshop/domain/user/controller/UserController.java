1952c67d0302a65a098bfe5932bcae107788fa3e
곽현민 <hunmin111@gmail.com>
2026-04-02 14:59:16 +0900

    feat: 회원가입 기능 controller 구현

--- src/main/java/com/kcfcoffeeshop/domain/user/controller/UserController.java
+++ src/main/java/com/kcfcoffeeshop/domain/user/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.kcfcoffeeshop.domain.user.controller;
 
+import com.kcfcoffeeshop.common.dto.BaseResponse;
+import com.kcfcoffeeshop.domain.user.dto.request.UserSignupRequest;
+import com.kcfcoffeeshop.domain.user.dto.response.UserSignupResponse;
 import com.kcfcoffeeshop.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<BaseResponse<UserSignupResponse>> userSignup(@Valid @RequestBody UserSignupRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                BaseResponse.success(HttpStatus.CREATED, "회원가입 성공", userService.userSignup(request))
+        );
+    }
 }
