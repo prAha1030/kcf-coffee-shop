20076ffc665fd8486ae82bbb610b8c31f766a775
곽현민 <hunmin111@gmail.com>
2026-04-02 12:11:08 +0900

    feat: UserController 구조 추가

---
+++ src/main/java/com/kcfcoffeeshop/domain/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.kcfcoffeeshop.domain.user.controller;
+
+import com.kcfcoffeeshop.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+}
