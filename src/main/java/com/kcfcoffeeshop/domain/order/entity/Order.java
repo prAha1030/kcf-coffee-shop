ba0a29431f7805871f88a4e2e3a68d5d2b0a90a7
곽현민 <hunmin111@gmail.com>
2026-04-02 10:19:52 +0900

    feat: order 엔티티 추가

---
+++ src/main/java/com/kcfcoffeeshop/domain/order/entity/Order.java
@@ -0,0 +1,34 @@
+package com.kcfcoffeeshop.domain.order.entity;
+
+import com.kcfcoffeeshop.common.entity.BaseEntity;
+import com.kcfcoffeeshop.domain.order.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String orderNumber;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+}
---
+++ src/main/java/com/kcfcoffeeshop/domain/order/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.kcfcoffeeshop.domain.order.enums;
+
+public enum OrderStatus {
+    PENDING, // 대기
+    SUCCESS, // 성공
+    REFUND   // 환불
+}
