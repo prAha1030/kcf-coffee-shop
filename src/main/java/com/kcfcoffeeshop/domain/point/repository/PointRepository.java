4f90eeb218cc5c42a3a0fbc5320a8ccdca4173ea
곽현민 <hunmin111@gmail.com>
2026-04-04 01:50:06 +0900

    feat: PointRepository userId로 포인트 조회 메서드 추가

--- src/main/java/com/kcfcoffeeshop/domain/point/repository/PointRepository.java
+++ src/main/java/com/kcfcoffeeshop/domain/point/repository/PointRepository.java
@@ -3,5 +3,9 @@ package com.kcfcoffeeshop.domain.point.repository;
 import com.kcfcoffeeshop.domain.point.entity.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PointRepository extends JpaRepository<Point, Long> {
+
+    Optional<Point> findByUserId(Long userId);
 }
