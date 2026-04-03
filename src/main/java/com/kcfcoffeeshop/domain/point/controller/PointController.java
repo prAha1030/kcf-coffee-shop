29274c2cbda62c7d416ff9e67f4380d888f74822
곽현민 <hunmin111@gmail.com>
2026-04-04 00:00:10 +0900

    feat: 포인트 충전 기능 controller 구현

---
+++ src/main/java/com/kcfcoffeeshop/domain/point/controller/PointController.java
@@ -0,0 +1,33 @@
+package com.kcfcoffeeshop.domain.point.controller;
+
+import com.kcfcoffeeshop.common.dto.BaseResponse;
+import com.kcfcoffeeshop.domain.point.dto.request.PointChargeRequest;
+import com.kcfcoffeeshop.domain.point.dto.response.PointChargeResponse;
+import com.kcfcoffeeshop.domain.point.service.PointService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/points")
+public class PointController {
+
+    private final PointService pointService;
+
+    @PostMapping
+    public ResponseEntity<BaseResponse<PointChargeResponse>> chargePoint(
+            @Valid @RequestBody PointChargeRequest request,
+            @AuthenticationPrincipal Long userId
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                BaseResponse.success(HttpStatus.CREATED, "포인트 충전 성공", pointService.chargePoint(request, userId))
+        );
+    }
+}
