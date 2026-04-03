975e2ad404eb7268e08a9444522a33962b10a00f
곽현민 <hunmin111@gmail.com>
2026-04-03 13:54:15 +0900

    feat: JwtFilter 추가

--- src/main/java/com/kcfcoffeeshop/common/config/security/SecurityConfig.java
+++ src/main/java/com/kcfcoffeeshop/common/config/security/SecurityConfig.java
@@ -10,11 +10,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,7 +28,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/users/signup").permitAll()
                         .anyRequest().authenticated())
-                // TODO jwt filter 추가
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
