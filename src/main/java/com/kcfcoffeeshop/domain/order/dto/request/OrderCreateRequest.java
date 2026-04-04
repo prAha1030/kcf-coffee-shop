5f2bc47c26604c4576ad47018f0c1ed61f021f35
곽현민 <hunmin111@gmail.com>
2026-04-05 00:44:03 +0900

    refactor: OrderCreateRequest의 quantity validation 수정

--- src/main/java/com/kcfcoffeeshop/domain/order/dto/request/OrderCreateRequest.java
+++ src/main/java/com/kcfcoffeeshop/domain/order/dto/request/OrderCreateRequest.java
@@ -1,5 +1,6 @@
 package com.kcfcoffeeshop.domain.order.dto.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -16,6 +17,7 @@ public record OrderCreateRequest(
             Long menuId,
 
             @Min(value = 1, message = "수량은 1개 이상이어야 합니다")
+            @Max(value = 100, message = "수량은 100개 이하여야 합니다")
             int quantity
     ) {
     }
