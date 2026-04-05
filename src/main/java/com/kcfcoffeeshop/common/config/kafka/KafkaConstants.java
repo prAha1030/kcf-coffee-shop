23fd8a41863fdedc6418bd8fa28c376adfbd2ffa
곽현민 <hunmin111@gmail.com>
2026-04-05 19:24:44 +0900

    feat: KafkaConstants에 처리된 주문 상수 추가

--- src/main/java/com/kcfcoffeeshop/common/config/kafka/KafkaConstants.java
+++ src/main/java/com/kcfcoffeeshop/common/config/kafka/KafkaConstants.java
@@ -4,4 +4,5 @@ public class KafkaConstants {
     public static final String ORDER_COMPLETE_TOPIC = "order.complete";
     public static final String ORDER_GROUP_ID = "order-group";
     public static final String STRING_LISTENER_FACTORY = "stringKafkaListenerContainerFactory";
+    public static final String ORDER_PROCESSED_PREFIX = "order:processed";
 }
