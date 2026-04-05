3c018fb03b0e81e386092657d011e6e722bd69b2
곽현민 <hunmin111@gmail.com>
2026-04-05 18:49:08 +0900

    feat: KafkaConstants 추가

---
+++ src/main/java/com/kcfcoffeeshop/common/config/kafka/KafkaConstants.java
@@ -0,0 +1,7 @@
+package com.kcfcoffeeshop.common.config.kafka;
+
+public class KafkaConstants {
+    public static final String ORDER_COMPLETE_TOPIC = "order.complete";
+    public static final String ORDER_GROUP_ID = "order-group";
+    public static final String STRING_LISTENER_FACTORY = "stringKafkaListenerContainerFactory";
+}
