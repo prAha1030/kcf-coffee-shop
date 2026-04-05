6446b9010d43524909195f542898cb16521980bd
곽현민 <hunmin111@gmail.com>
2026-04-05 12:14:08 +0900

    feat: MenuErrorCode enum 추가

---
+++ src/main/java/com/kcfcoffeeshop/domain/menu/enums/MenuErrorCode.java
@@ -0,0 +1,15 @@
+package com.kcfcoffeeshop.domain.menu.enums;
+
+import com.kcfcoffeeshop.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MenuErrorCode implements ErrorCode {
+    ERR_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 메뉴입니다");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
