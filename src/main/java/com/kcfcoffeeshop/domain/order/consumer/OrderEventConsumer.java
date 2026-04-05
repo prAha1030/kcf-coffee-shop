d1af45174fe7689fe6e7074daf7d0130b95e6ea9
곽현민 <hunmin111@gmail.com>
2026-04-05 18:55:13 +0900

    feat: OrderEventConsumer 기본 구조 및 이벤트 수신 추가

---
+++ src/main/java/com/kcfcoffeeshop/domain/order/consumer/OrderEventConsumer.java
@@ -0,0 +1,30 @@
+package com.kcfcoffeeshop.domain.order.consumer;
+
+import com.kcfcoffeeshop.common.config.kafka.KafkaConstants;
+import com.kcfcoffeeshop.domain.order.dto.kafka.OrderCompleteEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderEventConsumer {
+
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(
+            topics = KafkaConstants.ORDER_COMPLETE_TOPIC,
+            groupId = KafkaConstants.ORDER_GROUP_ID,
+            containerFactory = KafkaConstants.STRING_LISTENER_FACTORY
+    )
+    public void consumeOrderCompleteEvent(String message) {
+        // checked exception은 DLT 처리
+        OrderCompleteEvent event = objectMapper.readValue(message, new TypeReference<>() {});
+        log.info("Kafka 주문 성공 이벤트 수신 성공 : {}", event);
+        // 메뉴 랭킹 서비스 호출하여 랭킹 증가 메서드 호출
+    }
+}
